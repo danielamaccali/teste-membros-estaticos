9b950cd4ac4082c7ac7c7b6d967c3bb7e5df71be
Daniela Maccali <daniela.maccali@gmail.com>
2022-01-19 20:51:37 -0300

    first commit

---
+++ ExercMembEst1/CalculadoraDolar.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ExercMembEst1
+{
+    public static class CalculadoraDolar
+    {
+        public static double Iof = 6;
+
+        public static double Conversor(double cotacao, double valor)
+        {
+            double subtotal = (valor * cotacao);
+            return subtotal + (subtotal * (Iof/100) ) ;
+        }
+
+    }
+}
---
+++ ExercMembEst1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExercMembEst1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            /*Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por
+uma pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda
+que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. Criar uma classe ConversorDeMoeda
+para ser responsável pelos cálculos.*/
+        {
+            Console.WriteLine("Valor a ser comprado em U$: ");
+            double ValorCompra = double.Parse(Console.ReadLine());
+            Console.WriteLine("Cotação dólar: ");
+            double CotDolar = double.Parse(Console.ReadLine());
+
+            double ValorFinal = CalculadoraDolar.Conversor(CotDolar, ValorCompra);
+            Console.WriteLine($"Valor a ser pago em Reais: {ValorFinal}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
